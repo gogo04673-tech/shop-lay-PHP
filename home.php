65e369d0cea0bf6667aa05bbce61302b2417432d
Mohammed <eljihadmohammed84@gmail.com>
2025-08-09 22:56:21 +0100

    home

--- home.php
+++ home.php
@@ -6,7 +6,7 @@ $categories = getData("categories", null, false);
 $itemView = getData("items_view", "items_discount != 0", false);
 
 $allData['status'] = "success";
-//$allData['categories'] = $categories;
+$allData['categories'] = $categories;
 $allData['itemView'] = $itemView;
 
 echo json_encode($allData);
