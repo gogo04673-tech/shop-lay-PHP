7b0b0035c8ccf9074ab1d8af7245c0b09d4bb455
Mohammed <eljihadmohammed84@gmail.com>
2025-08-09 23:48:33 +0100

    add items

--- home.php
+++ home.php
@@ -3,10 +3,12 @@ include "./functions.php";
 
 $allData = [];
 $categories = getData("categories", null, false);
+$items = getData("items", null, false);
 $itemView = getData("items_view", "items_discount != 0", false);
 
 $allData['status'] = "success";
 $allData['categories'] = $categories;
 $allData['itemView'] = $itemView;
+$allData['items'] = $items;
 
 echo json_encode($allData);
