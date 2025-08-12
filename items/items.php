ba9d1bc8cae4cecb59784381161d37456481b395
Mohammed <eljihadmohammed84@gmail.com>
2025-08-12 17:13:59 +0100

    edit a items.php

--- items/items.php
+++ items/items.php
@@ -6,29 +6,61 @@ error_reporting(E_ALL);
 header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=UTF-8");
 
-include "../functions.php";
+include "../connect.php";
 
 $input = file_get_contents('php://input');
-$data = json_decode($input, true) ?: $_POST;
+$dataInput = json_decode($input, true) ?: $_POST;
 
-$categoryId = isset($data['categoryId']) ? $data['categoryId'] : '';
+$categoryId = isset($dataInput['categoryId']) ? intval($dataInput['categoryId']) : 0;
+$userId = isset($dataInput['userId']) ? intval($dataInput['userId']) : 0;
 
-if (empty($categoryId)) {
+if (empty($categoryId) || empty($userId)) {
     echo json_encode([
         "status" => "failure",
-        "message" => "categoryId is required"
+        "message" => "categoryId AND userId are required"
     ]);
+    exit;
 }
 
-// categoryId
+$sql = "
+    SELECT items_view.*, 1 as favorite 
+    FROM items_view 
+    INNER JOIN favorite 
+        ON favorite.favorite_item_id = items_view.items_id 
+        AND favorite.favorite_user_id = :userId
+    WHERE categories_id = :categoryId
 
-$allData = [];
+    UNION ALL
 
+    SELECT items_view.*, 0 as favorite 
+    FROM items_view
+    WHERE categories_id = :categoryId 
+      AND items_id NOT IN ( 
+            SELECT items_view.items_id 
+            FROM items_view 
+            INNER JOIN favorite 
+                ON favorite.favorite_item_id = items_view.items_id 
+                AND favorite.favorite_user_id = :userId
+      )
+";
 
-$items = getData("items_view", "categories_id = $categoryId", false);
+$stmt = $connect->prepare($sql);
+$stmt->bindValue(':userId', $userId, PDO::PARAM_INT);
+$stmt->bindValue(':categoryId', $categoryId, PDO::PARAM_INT);
 
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$allData['status'] = "success";
-$allData['items'] = $items;
-
-echo json_encode($allData);
+if ($result) {
+    echo json_encode([
+        "status" => "success",
+        "message" => "Items retrieved successfully",
+        "data" => $result
+    ]);
+} else {
+    echo json_encode([
+        "status" => "failure",
+        "message" => "No items found",
+        "data" => []
+    ]);
+}
