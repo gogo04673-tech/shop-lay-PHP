2d438e3e2d7e9cdc85b014ae48a07682ce794629
Mohammed <eljihadmohammed84@gmail.com>
2025-08-14 21:11:06 +0100

    add view.php cart

---
+++ cart/view.php
@@ -0,0 +1,52 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json; charset=UTF-8");
+
+include "../functions.php";
+include "../connect.php";
+
+$input = file_get_contents('php://input');
+$data = json_decode($input, true) ?: $_POST;
+
+$userId = isset($data['userId']) ? intval($data['userId']) : 0;
+
+
+if (empty($userId)) {
+    echo json_encode([
+        "status" => "failure",
+        "message" => "userId are required"
+    ]);
+    exit;
+}
+
+$data = getAllData("items_cart", null, false);
+
+$sql = "
+SELECT 
+SUM(items_cart.total) as total_price_items, 
+COUNT(items_cart.count_item) as total_count_items 
+FROM `items_cart` 
+WHERE items_cart.cart_users_id = ? 
+GROUP BY cart_users_id
+";
+$stmt = $connect->prepare($sql);
+$stmt->execute([$userId]);
+$dataCountAndPrice = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($stmt->rowCount() > 0) {
+    echo json_encode([
+        "status" => "success",
+        "message" => "Items cart and data count and price are found.",
+        "data" => $data,
+        "countPrice" => $dataCountAndPrice
+    ]);
+} else {
+    echo json_encode([
+        "status" => "failure",
+        "message" => "Items cart and data count and price aren't found."
+    ]);
+}
--- views.sql
+++ views.sql
@@ -9,3 +9,23 @@ CREATE OR REPLACE VIEW favorite_items AS
 SELECT items.*, favorite.*, users.* FROM favorite
 INNER JOIN users ON users.users_id = favorite.favorite_user_id
 INNER JOIN items ON items.items_id = favorite.favorite_item_id
+
+// View items cart 
+CREATE OR REPLACE VIEW items_cart AS
+SELECT 
+    SUM(items.items_price) AS total,
+    COUNT(cart.cart_items_id) AS count_item,
+    MAX(cart.cart_id) AS cart_id,
+    cart.cart_users_id,
+    cart.cart_items_id,
+    MAX(items.items_id) AS items_id,
+    MAX(items.items_name) AS items_name,
+    MAX(items.items_name_ar) AS items_name_ar,
+    MAX(items.items_price) AS items_price,
+    MAX(items.items_image) AS items_image,
+    MAX(items.items_desc) AS items_desc,
+    MAX(items.items_desc_ar) AS items_desc_ar
+FROM cart
+JOIN items 
+    ON cart.cart_items_id = items.items_id
+GROUP BY cart.cart_items_id, cart.cart_users_id;
