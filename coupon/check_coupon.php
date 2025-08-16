8dcc42c5a38a363bd47324a6daa8f3915ddb5c0f
Mohammed <eljihadmohammed84@gmail.com>
2025-08-16 23:43:41 +0100

    add check coupon

---
+++ coupon/check_coupon.php
@@ -0,0 +1,27 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json; charset=UTF-8");
+
+include "../functions.php";
+
+$input = file_get_contents('php://input');
+$data = json_decode($input, true) ?: $_POST;
+
+
+$couponName = isset($data['couponName']) ? trim($data['couponName']) : '';
+$now = date("Y-m-d H:i:s");
+
+
+if ($couponName === '') {
+    echo json_encode([
+        "status" => "failure",
+        "message" => "couponName is required"
+    ]);
+    exit;
+}
+
+getData("coupon", "coupon_name = $couponName AND coupon_expire_date > $now AND coupon_count > 0 ");
--- functions.php
+++ functions.php
@@ -234,7 +234,7 @@ function getData($table, $where = null, $json = true)
 	include __DIR__ . "/connect.php";
 
 	try {
-		$allowedTables = ['categories', 'users', 'items', 'items_view', 'favorite_items', 'cart', 'items_cart', 'address'];
+		$allowedTables = ['categories', 'users', 'items', 'items_view', 'favorite_items', 'cart', 'items_cart', 'address', 'coupon'];
 		if (!in_array($table, $allowedTables)) {
 			throw new Exception("Invalid table name");
 		}
