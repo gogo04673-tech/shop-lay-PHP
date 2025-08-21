d5004cb0a9cf6ec412ef3eeb009b9ff42c0e71a3
Mohammed <eljihadmohammed84@gmail.com>
2025-08-21 20:02:50 +0100

    edit notifications.php

--- notifications.php
+++ notifications.php
@@ -6,17 +6,21 @@ error_reporting(E_ALL);
 header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=UTF-8");
 
-
 include "./functions.php";
+
+// استلام البيانات
+$input = file_get_contents("php://input");
+$data = json_decode($input, true) ?: $_POST;
 
 $userId  = isset($data['userId']) ? intval($data['userId']) : 0;
 
 if ($userId == 0) {
-    echo array(
+    echo json_encode([
         "status" => "Failure",
         "message" => "User id is required"
-    );
+    ]);
     exit();
 }
 
-getAllData('notifications', "notifications_users_id = $userId ORDER BY $userId DESC");
+// جلب البيانات
+getAllData('notifications', "notifications_users_id = $userId ORDER BY notifications_id DESC");
