5901df5d5f3f6c86829633d665ad6d430fbd6ab2
Mohammed <eljihadmohammed84@gmail.com>
2025-08-21 16:16:54 +0100

    Edit file approve.php

--- admin/orders/approve.php
+++ admin/orders/approve.php
@@ -9,15 +9,25 @@ header("Content-Type: application/json; charset=UTF-8");
 include "../../connect.php";
 include "../../functions.php";
 
-$userId = isset($data['userId']) ? intval($data['userId']) : 0;
+// استلام البيانات من JSON أو POST
+$input = file_get_contents('php://input');
+$data = json_decode($input, true) ?: $_POST;
+
+$userId  = isset($data['userId']) ? intval($data['userId']) : 0;
 $orderId = isset($data['orderId']) ? intval($data['orderId']) : 0;
 
-$data = array(
-    "orders_status" => 1,
-);
+if ($orderId > 0 && $userId > 0) {
+    // تحديث حالة الطلب
+    $updateData = [
+        "orders_status" => 1
+    ];
 
-updateData('orders', $data, ["orders_id" => $orderId, "orders_status" => 0]);
+    updateData('orders', $updateData, ["orders_id" => $orderId, "orders_status" => 0]);
 
+    // إرسال إشعار
+    sendGCM("Success", "Your order is preparing.", "users$userId", "none", "none");
 
-
-sendGCM("Success", "Your order is preparing.", "users$userId", "none", "none");
+    echo json_encode(["status" => "success"]);
+} else {
+    echo json_encode(["status" => "error", "message" => "Missing userId or orderId"]);
+}
