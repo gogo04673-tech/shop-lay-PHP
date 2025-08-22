3485f99a1edca5590e9166e0a3cb903fbf111847
Mohammed <eljihadmohammed84@gmail.com>
2025-08-23 00:27:45 +0100

    edit offers.php

--- items/offers.php
+++ items/offers.php
@@ -6,7 +6,47 @@ error_reporting(E_ALL);
 header('Access-Control-Allow-Origin: *');
 header("Content-Type: application/json; charset=UTF-8");
 
-include "../functions.php";
+include "../connect.php";
 
 
-getAllData("items", "items_discount != 0");
+$sql = "
+    SELECT items_view.*, 1 as favorite, 
+    (items_price - (items_price * items_discount / 100)) as items_price_discount
+    FROM items_view 
+    INNER JOIN favorite 
+        ON favorite.favorite_item_id = items_view.items_id 
+        
+    WHERE items_discount != 0 
+
+    UNION ALL
+
+    SELECT items_view.*, 0 as favorite,
+    (items_price - (items_price * items_discount / 100)) as items_price_discount 
+    FROM items_view
+    WHERE items_discount != 0
+      AND items_id NOT IN ( 
+            SELECT items_view.items_id 
+            FROM items_view 
+            INNER JOIN favorite 
+                ON favorite.favorite_item_id = items_view.items_id 
+                
+      )
+";
+
+$stmt = $connect->prepare($sql);
+
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if ($result) {
+    echo json_encode([
+        "status" => "success",
+        "message" => "Items retrieved successfully",
+        "data" => $result
+    ]);
+} else {
+    echo json_encode([
+        "status" => "failure",
+        "message" => "No items found",
+    ]);
+}
