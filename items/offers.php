63430f6e700a23fbddd0e8a617cc635aeca7adb3
Mohammed <eljihadmohammed84@gmail.com>
2025-08-23 00:12:36 +0100

    add file offers.php in folder items.

---
+++ items/offers.php
@@ -0,0 +1,12 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json; charset=UTF-8");
+
+include "../functions.php";
+
+
+getAllData("items", "items_discount != 0");
