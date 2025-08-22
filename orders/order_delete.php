fbfd0d74984443e4d885c1b28453d5db1333ece8
Mohammed <eljihadmohammed84@gmail.com>
2025-08-22 23:09:53 +0100

    add file delete order

---
+++ orders/order_delete.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json; charset=UTF-8");
+
+include "../connect.php";
+
+$input = file_get_contents('php://input');
+$data = json_decode($input, true) ?: $_POST;
+
+$orderId = isset($data['orderId']) ? intval($data['orderId']) : 0;
+
+if ($orderId <= 0) {
+    echo json_encode([
+        "status" => "failure",
+        "message" => "addressId is required and must be greater than 0"
+    ]);
+    exit();
+}
+
+$stmt = $connect->prepare("DELETE FROM `orders` WHERE orders_id = ?");
+$stmt->execute([$orderId]);
+
+if ($stmt->rowCount() > 0) {
+    echo json_encode([
+        "status" => "success",
+        "message" => "Order deleted successfully"
+    ]);
+} else {
+    echo json_encode([
+        "status" => "failure",
+        "message" => "No order found with this ID"
+    ]);
+}
