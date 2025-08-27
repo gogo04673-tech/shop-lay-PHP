00311a595c0d1775551d629d77230be8fbe5e4c7
Eljihad Mohammed <eljihadmohammed84@gmail.com>
2025-08-27 19:10:51 +0100

    Update index.php
    I will add index.php edit

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<?php
 
+require_once 'connect.php';
+
+require_once 'functions.php';
+
+$page = isset($_GET['page'] ? $_GET['page'] : 'home' ;
+
+switch($page) {
+  case 'signup': 
+    require 'signup.ph';
+    break;
+  case 'login':
+    require 'signin.php';
+    break;
+  default: 
+    require 'home.php';
+    break;
+}
